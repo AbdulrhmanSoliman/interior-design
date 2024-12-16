3542743866139d1efad4b12b4df5b1b5053ef743
Abdulrhman Soliman <abdo.soliman156@gmail.com>
2024-12-16 14:51:46 +0200

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   darkMode: "selector",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "0.5rem",
+    },
     colors: {
       primary: "#1f1f1f",
       secondary: "#545454",
